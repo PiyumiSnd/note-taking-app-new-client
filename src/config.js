3deb554ea93d24d334a48c724a5b13b4906fd7dd
piyumi ranasinghe <piyumi.ranasinghe@itelasoft.com>
2019-12-30 09:23:40 +0530

    Setting up react app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-2",
+      BUCKET: "notes-uploads-notes-taking-app"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "https://c2e2evblpa.execute-api.us-east-2.amazonaws.com/dev"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_uINzCPQ9M",
+      APP_CLIENT_ID: "1eik4k49ah2go8ftj2l1dtl756",
+      IDENTITY_POOL_ID: "us-east-2:77c5c706-79b3-4495-a8b0-0e01e0c76526"
+    }
+  };
